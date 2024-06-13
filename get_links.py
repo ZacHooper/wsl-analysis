cf46219525f52a2fb4a44a965329aaf9133a715b
Zac Hooper <zac.g.hooper@gmail.com>
2024-06-13 20:11:35 +1000

    refactor: soup parsing into Event object

--- get_links.py
+++ get_links.py
@@ -22,39 +22,49 @@ class Event:
             "event_location": self.event_location,
         }
 
+    def from_soup(soup):
+        event_window = soup.find("td", class_="event-date-range").text
+        event_name = soup.find("a", class_="event-schedule-details__event-name")
+        # remove span from event_name
+        try:
+            event_name.span.decompose()
+        except AttributeError:
+            pass
+        event_name = event_name.text
+        event_link = (
+            soup.find("a", class_="event-schedule-details__event-name")["href"]
+            .replace("main", "results")
+            .replace("event", "results")
+        )
+        event_location = soup.find(
+            "span", class_="event-schedule-details__location"
+        ).text
+        return Event(event_window, event_name, event_link, event_location)
 
-year = 2024
-url = f"https://www.worldsurfleague.com/events/{year}/ct?all=1"
 
-r = requests.get(url)
-soup = BeautifulSoup(r.text, "html.parser")
+def get_event_links(year: int) -> None:
+    url = f"https://www.worldsurfleague.com/events/{year}/ct?all=1"
 
-event_table = soup.find("table", class_="tableType-event")
-tbody = event_table.find("tbody")
-event_rows = tbody.find_all("tr", class_=re.compile("^event-\d+"))
+    # Load Page
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
 
-events = []
-for event in event_rows:
-    event_window = event.find("td", class_="event-date-range").text
-    event_name = event.find("a", class_="event-schedule-details__event-name")
-    # remove span from event_name
-    try:
-        event_name.span.decompose()
-    except AttributeError:
-        pass
-    event_name = event_name.text
-    event_link = (
-        event.find("a", class_="event-schedule-details__event-name")["href"]
-        .replace("main", "results")
-        .replace("event", "results")
-    )
-    event_location = event.find("span", class_="event-schedule-details__location").text
-    events.append(Event(event_window, event_name, event_link, event_location))
+    # Find all event elements
+    event_table = soup.find("table", class_="tableType-event")
+    tbody = event_table.find("tbody")
+    event_rows = tbody.find_all("tr", class_=re.compile("^event-\d+"))
 
-# Write to csv
-keys = events[0].to_dict().keys()
+    # Parse events
+    events = [Event.from_soup(event) for event in event_rows]
 
-with open("events.csv", "w", newline="") as output_file:
-    dict_writer = csv.DictWriter(output_file, keys)
-    dict_writer.writeheader()
-    dict_writer.writerows([event.to_dict() for event in events])
+    # Write to csv
+    keys = events[0].to_dict().keys()
+
+    with open("events.csv", "w", newline="") as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows([event.to_dict() for event in events])
+
+
+if __name__ == "__main__":
+    get_event_links(2023)
